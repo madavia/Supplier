39e3e4d5c107b315edad89d4167bb0d813beb94f
Arava Madhavilatha <marava@hagroup.com>
2023-09-12 21:33:13 +0530

    FirstGitFile

---
+++ SupplierAPI/Contexts/SupplierContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupplierAPI.Models;
+
+namespace SupplierAPI.Contexts
+{
+    public class SupplierContext:DbContext
+    {
+        public SupplierContext(DbContextOptions opts) : base(opts)
+        {
+
+        }
+        public DbSet<Supplier> suppliers { get; set; }
+        public DbSet<Product> products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           // modelBuilder.Entity<Supplier>().HasKey(es => new { es.Id, es.Email });
+
+            modelBuilder.Entity<Supplier>().HasData(
+                new Supplier { Id = 101, Name = "Ramu", Email = "Ramu@gmail.com", Phone = "1234567890"},
+                new Supplier { Id = 102, Name = "Somu", Email = "Somu@gmail.com", Phone = "9876543210"}
+
+                );
+           
+
+        }
+
+    }
+}
